9e25659ede33765477bb1fcb3ad49d93fb9a20fc
Paul DANET <danet.paul@orange.fr>
2024-10-04 15:27:04 +0200

    Add experiences, projects and aboutMe

---
+++ src/components/Header.jsx
@@ -0,0 +1,199 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+export default function Header() {
+  useGSAP(() => {
+    const timeline = gsap.timeline();
+
+    // Animation du cercle dessiné à la main
+    gsap.to(".circle-svg", {
+      strokeDashoffset: 0,
+      duration: 1,
+      ease: "power1.inOut",
+      delay: 3,
+    });
+
+    // Animation de l'invasion de couleur
+    gsap.to(".color-bg", {
+      height: "100vh",
+      duration: 1,
+      delay: 1,
+      ease: "power3.in",
+    });
+
+    // Animation du prénom en intro
+    gsap.from(".first-name", {
+      y: "10vw",
+      duration: 2,
+      ease: "power.out",
+      onComplete: () => {
+        gsap.to(".first-name, .last-name", { color: "#111111" });
+      },
+    });
+
+    // Animation du nom en intro
+    gsap.from(".last-name", {
+      y: "-10vw",
+      duration: 2,
+      ease: "power.out",
+    });
+
+    // Animation des mots qui sortent du sol en intro
+    gsap.fromTo(
+      ".intro-word",
+      {
+        y: "10vw",
+        clipPath: "inset(0 0 100% 0)",
+      },
+      {
+        y: "0vw",
+        clipPath: "inset(0 0 0% 0)",
+        opacity: 1,
+        duration: 1,
+        ease: "power.out",
+        stagger: 0.1,
+        delay: 2,
+      }
+    );
+
+    // Animation des lettres du mot "Brittany" qui apparaissent
+    gsap.fromTo(
+      ".brittany-letters .letter",
+      {
+        y: "10vw",
+        clipPath: "inset(0 0 100% 0)",
+      },
+      {
+        y: "0vw",
+        clipPath: "inset(0 0 0% 0)",
+        opacity: 1,
+        duration: 1,
+        ease: "power.out",
+        delay: 3,
+      }
+    );
+
+    // Animation des lettres du mot "Brittany" qui disparaissent
+    timeline.to(".brittany-letters .letter", {
+      y: "-100%",
+      clipPath: "inset(0 0 100% 0)",
+      stagger: 0.03,
+      ease: "power2.in",
+      delay: 4,
+    });
+
+    // Animation des lettres du mot "Lorient" qui apparaissent
+    timeline.fromTo(
+      ".lorient-letters .letter",
+      {
+        y: "100%",
+        clipPath: "inset(0 0 100% 0)",
+      },
+      {
+        y: "0%",
+        clipPath: "inset(0 0 0% 0)",
+        opacity: 1,
+        stagger: 0.03,
+        ease: "power2.out",
+      },
+      "-=0.3" // Décalage pour chevaucher l'animation précédente
+    );
+
+    // Animation du soulignement en intro
+    gsap.to(".underline", {
+      scaleX: 1,
+      duration: 1,
+      delay: 4,
+      ease: "power2.in",
+    });
+
+    // Animation du soulignement lors du hover sur l'adresse email
+    document
+      .querySelector(".email-hover")
+      .addEventListener("mouseenter", () => {
+        gsap.fromTo(
+          ".underline",
+          {
+            scaleX: 1,
+            duration: 1,
+          },
+          {
+            scaleX: 0,
+            duration: 1,
+            ease: "power2.in",
+            onComplete: () => {
+              gsap.to(".underline", {
+                scaleX: 1,
+                duration: 1,
+                ease: "power2.out",
+              });
+            },
+          }
+        );
+      });
+  });
+  return (
+    <>
+      <div className="color-bg absolute bottom-0 left-0 w-screen h-0 bg-[#f0f0f0] -z-10"></div>
+      <div className="intro-section font-['canela'] text-[5vw] leading-normal font-medium">
+        <div>
+          <span className="intro-word mr-1.5">Web </span>
+          <span className="intro-word mr-1.5">Developer </span>
+          <span className="intro-word mr-1.5">Available </span>
+          <span className="intro-word mr-1.5">for </span>
+          <div className="relative inline-block">
+            <span className="intro-word mr-1.5">your </span>
+            <span className="intro-word mr-1.5">Project</span>
+            <svg
+              className="circle-svg absolute -top-2 -left-1.5 stroke-[#111111] z-10 w-[110%] h-[110%] [filter: url(#fuzzy)] [stroke-dasharray:1259] [stroke-dashoffset:1259]"
+              fill="none"
+              viewBox="0 0 350 80"
+            >
+              <path d="M122.5 1.5C78.5-2.2 1.6 6.7 6 39.5c5.5 41 90 40 158.5 44 32 1.9 201 12 178.5-38.5C325 4.5 139 6 120.5 6S-3.5 2.5 1 35.5s126 52.5 178.5 54S347 99 347 35.5"></path>
+            </svg>
+          </div>
+        </div>
+        <div className="flex">
+          <span className="intro-word mr-1.5">Made </span>
+          <span className="intro-word mr-1.5">In </span>
+          <div className="relative">
+            <div className="brittany-letters flex absolute top-0 left-0 w-full">
+              <span className="letter">B</span>
+              <span className="letter">r</span>
+              <span className="letter">i</span>
+              <span className="letter">t</span>
+              <span className="letter">t</span>
+              <span className="letter">a</span>
+              <span className="letter">n</span>
+              <span className="letter">y</span>
+            </div>
+            <div className="lorient-letters flex absolute top-0 left-0 w-full">
+              <span className="letter">L</span>
+              <span className="letter">o</span>
+              <span className="letter">r</span>
+              <span className="letter">i</span>
+              <span className="letter">e</span>
+              <span className="letter">n</span>
+              <span className="letter">t</span>
+            </div>
+          </div>
+        </div>
+        <div className="relative inline-block cursor-pointer">
+          <h1 className="email-hover">
+            <span className="intro-word">hello</span>
+            <span className="intro-word">@</span>
+            <span className="first-name relative inline-block text-[#f0f0f0]">
+              paul
+            </span>
+            <span className="last-name relative inline-block text-[#f0f0f0]">
+              danet
+            </span>
+            <span className="intro-word">.</span>
+            <span className="intro-word">com</span>
+          </h1>
+          <span className="underline inline-block w-full h-[0.5px] bg-[#111111] absolute left-0 bottom-1 scale-x-0 origin-left"></span>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/Projects.jsx
@@ -0,0 +1,96 @@
+import { useGSAP } from "@gsap/react";
+import { useEffect } from "react";
+import { gsap } from "gsap";
+
+export default function Projects() {
+  useEffect(() => {
+    const handleMouseEnter = () => {
+      gsap.to("circle", {
+        strokeDashoffset: 95,
+        strokeDasharray: 95,
+        duration: 2,
+        ease: "power2.inOut",
+      });
+
+      gsap.to("path", {
+        strokeDashoffset: 0,
+        duration: 1.5,
+        stagger: 0.3,
+        ease: "power2.inOut",
+      });
+    };
+    const handleMouseLeave = () => {
+      gsap.to("circle", {
+        strokeDashoffset: 0,
+        strokeDasharray: 0,
+        duration: 2,
+        ease: "power2.inOut",
+      });
+
+      gsap.to(".arrow-body", {
+        strokeDashoffset: 58,
+        duration: 1.5,
+        stagger: 0.3,
+        ease: "power2.inOut",
+      });
+      gsap.to(".arrow-head", {
+        strokeDashoffset: 21,
+        duration: 1.5,
+        stagger: 0.3,
+        ease: "power2.inOut",
+      });
+    };
+    const buttonElement = document.querySelector(".experiences-image");
+    buttonElement.addEventListener("mouseenter", handleMouseEnter);
+    buttonElement.addEventListener("mouseleave", handleMouseLeave);
+  }, []); // [] assure que l'effet ne s'exécute qu'une seule fois
+
+  return (
+    <>
+      <div className="experiences-image cursor-pointer flex justify-center absolute bottom-[5vw] right-[100px]">
+        <div className="">
+          <img
+            src="src/assets/50721.jpg"
+            className="rounded-xl w-[28vw] rotate-6"
+          ></img>
+        </div>
+        <div className="relative">
+          <svg
+            height="32"
+            width="32"
+            viewBox="0 0 42 42"
+            className="absolute bottom-11 -right-9"
+          >
+            <circle
+              className="stroke-[#111111] [stroke-dasharray:0] [stroke-dashoffset:0] stroke-1"
+              fill="none"
+              r="20"
+              cx="21"
+              cy="21"
+            ></circle>
+          </svg>
+          <svg
+            height="40"
+            width="40"
+            viewBox="0 0 42 42"
+            className="absolute bottom-6 -right-6
+            "
+          >
+            <path
+              d="M21 .5h20.5"
+              className="arrow-head stroke-[#111111] [stroke-dasharray:16] [stroke-dashoffset:21]"
+            ></path>
+            <path
+              d="M41 0.5v20.5"
+              className="arrow-head stroke-[#111111] [stroke-dasharray:30] [stroke-dashoffset:21]"
+            ></path>
+            <path
+              d="M.5 41 41 .5"
+              className="arrow-body stroke-[#111111] [stroke-dasharray:58] [stroke-dashoffset:58]"
+            ></path>
+          </svg>
+        </div>
+      </div>
+    </>
+  );
+}
