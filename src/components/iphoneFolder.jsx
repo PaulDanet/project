bb427e9d604fac8da40636f05e58fe0a1d9d1cb2
Paul DANET <danet.paul@orange.fr>
2024-11-03 17:58:29 +0100

    add folder for toolbox

--- src/components/iphoneFolder.jsx
+++ src/components/iphoneFolder.jsx
@@ -11,24 +11,40 @@ const IphoneFolder = ({ appList, title }) => {
   const blurBackgroundRef = useRef(null);
 
   const openFolder = () => {
-    gsap.to(folderRef.current, {
-      duration: 0.4,
-      scale: 2,
-      ease: "linear",
-      transformOrigin: "center center",
+    // Obtenir la position initiale de l'élément
+    const folderRect = folderRef.current.getBoundingClientRect();
+    gsap.to(titleBottomRef.current, {
+      opacity: 0,
     });
+    gsap.fromTo(
+      folderRef.current,
+      {
+        x: folderRect.left + folderRect.width / 2,
+        y: folderRect.top + folderRect.height / 2,
+      },
+      {
+        duration: 0.4,
+        position: "absolute",
+        zIndex: 100,
+        scale: 2,
+        transformOrigin: "center center",
+        top: "50%",
+        left: "50%",
+        x: "-50%",
+        y: "-50%",
+        ease: "linear",
+      }
+    );
 
     gsap.to(titleTopRef.current, {
       opacity: 1,
       duration: 0.4,
-    });
-
-    gsap.to(titleBottomRef.current, {
-      opacity: 0,
+      zIndex: 100,
     });
 
     gsap.to(blurBackgroundRef.current, {
       duration: 0.4,
+      zIndex: 90,
       backdropFilter: "blur(8px)",
       ease: "linear",
     });
@@ -37,15 +53,32 @@ const IphoneFolder = ({ appList, title }) => {
   };
 
   const closeFolder = () => {
-    gsap.to(folderRef.current, {
-      duration: 0.4,
-      scale: 1,
-      ease: "linear",
-    });
+    gsap.fromTo(
+      folderRef.current,
+      {
+        transformOrigin: "center center",
+        top: "50%",
+        left: "50%",
+        x: "-50%",
+        y: "-50%",
+      },
+      {
+        duration: 0.4,
+        scale: 1,
+        position: "",
+        zIndex: 50,
+        top: "0%",
+        left: "0%",
+        x: "-0%",
+        y: "-0%",
+        ease: "linear",
+      }
+    );
 
     gsap.to(titleTopRef.current, {
       opacity: 0,
       duration: 0.4,
+      zIndex: 0,
     });
 
     gsap.to(titleBottomRef.current, {
@@ -56,6 +89,7 @@ const IphoneFolder = ({ appList, title }) => {
 
     gsap.to(blurBackgroundRef.current, {
       duration: 0.4,
+      zIndex: -10,
       backdropFilter: "blur(0px)",
       ease: "linear",
     });
@@ -67,15 +101,15 @@ const IphoneFolder = ({ appList, title }) => {
     <div className="flex flex-col justify-center items-center gap-2">
       <div
         ref={titleTopRef}
-        className="opacity-0 absolute top-0 text-4xl text-black z-50"
+        className="opacity-0 absolute top-[10vh] right-1/2 translate-x-1/2 text-4xl text-black"
       >
         {title}
       </div>
-      <div ref={blurBackgroundRef} className="absolute inset-0" />
+      <div ref={blurBackgroundRef} className="absolute inset-0 -z-10" />
       <div
         ref={folderRef}
         onClick={isOpen ? closeFolder : openFolder}
-        className="bg-white/50 p-4 rounded-2xl shadow-lg cursor-pointer min-h-36 w-auto z-50"
+        className="bg-white/90 p-4 rounded-2xl shadow-lg cursor-pointer min-h-36 w-auto"
       >
         <div className={isOpen ? "m-4" : "m-0"}>
           <div
